f2445ca758929f2d3ff637cdd999df3d29738fb0
Dương Khánh <105584079+khanhmvp@users.noreply.github.com>
2026-07-31 23:00:24 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,19 +1,20 @@
 <?php
-// data.php
+
+declare(strict_types=1);
 
 $categories = [
-    1 => 'Ban phim',
-    2 => 'Man hinh',
-    3 => 'Chuot'
+    ['id' => 1, 'name' => 'Ban phim'],
+    ['id' => 2, 'name' => 'Chuot'],
+    ['id' => 3, 'name' => 'Man hinh'],
 ];
 
 $products = [
-    ['id' => 1, 'category_id' => 1, 'sku' => 'KB-01', 'name' => 'Bàn phím KB-01',  'price' => 1800000, 'qty' => 3],
-    ['id' => 2, 'category_id' => 1, 'sku' => 'KB-02', 'name' => 'Bàn phím KB-02',  'price' => 1200000, 'qty' => 5],
-    ['id' => 3, 'category_id' => 1, 'sku' => 'KB-03', 'name' => 'Bàn phím KB-03',  'price' => 1500000, 'qty' => 2],
-    ['id' => 4, 'category_id' => 3, 'sku' => 'MS-01', 'name' => 'Chuột MS-01',     'price' => 800000,  'qty' => 10],
-    ['id' => 5, 'category_id' => 3, 'sku' => 'MS-02', 'name' => 'Chuột MS-02',     'price' => 2500000, 'qty' => 4],
-    ['id' => 6, 'category_id' => 3, 'sku' => 'MS-03', 'name' => 'Chuột MS-03',     'price' => 1200000, 'qty' => 8],
-    ['id' => 7, 'category_id' => 2, 'sku' => 'MN-01', 'name' => 'Màn hình MN-01', 'price' => 5500000, 'qty' => 2],
-    ['id' => 8, 'category_id' => 2, 'sku' => 'MN-02', 'name' => 'LG UltraFine',   'price' => 8500000, 'qty' => 1],
+    ['sku' => 'KB-01', 'name' => 'Keychron K2', 'category_id' => 1, 'price' => 1890000, 'qty' => 3],
+    ['sku' => 'KB-02', 'name' => 'Akko 3087', 'category_id' => 1, 'price' => 1290000, 'qty' => 5],
+    ['sku' => 'MS-01', 'name' => 'Logitech M331', 'category_id' => 2, 'price' => 290000, 'qty' => 10],
+    ['sku' => 'MS-02', 'name' => 'Razer Viper', 'category_id' => 2, 'price' => 990000, 'qty' => 4],
+    ['sku' => 'MS-03', 'name' => 'Xiaomi Silent', 'category_id' => 2, 'price' => 250000, 'qty' => 8],
+    ['sku' => 'MN-01', 'name' => 'Dell 24 inch', 'category_id' => 3, 'price' => 3200000, 'qty' => 2],
+    ['sku' => 'MN-02', 'name' => 'LG UltraFine', 'category_id' => 3, 'price' => 8500000, 'qty' => 1],
+    ['sku' => 'KB-03', 'name' => 'Leopold FC660M', 'category_id' => 1, 'price' => 2750000, 'qty' => 2],
 ];
